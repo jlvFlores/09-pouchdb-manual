70d14e37b5695b62336364862c9030a788af048b
jlvFlores <jluisvazquez.jv@gmail.com>
2022-10-20 20:07:19 -0500

    97. Resolucion de la tarea - PouchDB

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 // 1- Crear la base de datos
 // Nombre:  mensajes
-
+let db = new PouchDB('mensajes'); 
 
 
 // Objeto a grabar en base de datos
@@ -17,18 +17,34 @@ let mensaje = {
 
 
 // 2- Insertar en la base de datos
-
+db.put( mensaje ).then( console.log('Registro insertado') );
 
 
 // 3- Leer todos los mensajes offline
+db.allDocs({ include_docs: true, decending: false })
+    .then( docs => {
 
+        "use strict";
 
+        console.log(docs.rows);
+    });
 
 
 // 4- Cambiar el valor 'sincronizado' de todos los objetos
 //  en la BD a TRUE
+db.allDocs({ include_docs: true, decending: false })
+    .then( docs => {
 
+        "use strict";
 
+        docs.rows.forEach( row => {
+            // console.log(row.doc);
+            let doc = row.doc;
+            doc.sincronizado = true;
+
+            db.put( doc );
+        });
+    });
 
 
 // 5- Borrar todos los registros, uno por uno, evaluando
@@ -36,7 +52,17 @@ let mensaje = {
 // deberá de comentar todo el código que actualiza
 // el campo de la sincronización 
 
+db.allDocs({ include_docs: true }).then( docs => {
 
+    "use strict";
 
+    docs.rows.forEach( row => {
 
+        let doc = row.doc;
+        if ( doc.sincronizado ) {
+            db.remove( doc );
+        }
 
+    });
+
+});
